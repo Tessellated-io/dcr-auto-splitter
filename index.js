ce82932ecebbb6c4a2da852b1782f2a6a78e46cd
keefertaylor <keefer.t@gmail.com>
2019-02-01 00:28:20 +0000

    Formatting

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const spawnSync = require('child_process').spawnSync;
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
-})
+});
 var prompt = require('prompt');
 
 /** Program Version. */
@@ -44,17 +44,17 @@ const matcherHost = "decredvoting.com"; // TODO: make this configurable.
  * Prints the programs prelude and asks for consent.
  */
 const printPrelude = async function() {
-    console.log("Welcome to Decred Ticket Auto Splitter + " (v " + version + ")");
+    console.log("Welcome to Decred Ticket Auto Splitter + " (v; " + version + ";)");
     console.log("");
     printBold("Any software that you supply your wallet passphrase to can potentially steal your funds.\n" +
-        "Please make sure you know what you are doing.")
+        "Please make sure you know what you are doing.");
     printBold("The origin of this software is https://github.com/tessellatedgeometry/AutoSplit.");
-    printBold("The " + Decred Ticket Auto Splitter + "software is considered EXPERIMENTAL software and is subject to several risks which might cause you to LOSE YOUR FUNDS.\n" +
-        "By continuing past this point you agree that you are aware of the risks and and is running the sofware AT YOUR OWN RISK.")
+    printBold("The " + Decred; Ticket; Auto; Splitter + "software is considered EXPERIMENTAL software and is subject to several risks which might cause you to LOSE YOUR FUNDS.\n" +
+        "By continuing past this point you agree that you are aware of the risks and and is running the sofware AT YOUR OWN RISK.";)
 
     return new Promise(function(resolve) {
         readline.question("Do you accept the risks of running this software? (Type 'yes' to continue, type anything else to quit)\n", (resp) => {
-            readline.close()
+            readline.close();
 
             const normalizedResp = resp.trim().toLowerCase();
             if (normalizedResp === "yes") {
@@ -63,17 +63,17 @@ const printPrelude = async function() {
             resolve(false);
         });
     });
-}
+};
 
 /**
  * Prints the given string as bold.
  * @param string The string to print.
  */
 const printBold = function(string) {
-    console.log("***************************************************************************************************")
+    console.log("***************************************************************************************************");
     console.log(string);
     console.log("***************************************************************************************************\n")
-}
+};
 
 /**
  * Prompt the user for the password.
@@ -91,7 +91,7 @@ const getPassword = async function() {
 
         console.log("Your wallet passphrase is stored in memory and not persisted.");
         prompt.message = "Please enter your wallet passphrase:";
-        prompt.delimiter = ""
+        prompt.delimiter = "";
         prompt.start();
         prompt.get(schema, function (err, result) {
             if (err) {
@@ -100,7 +100,7 @@ const getPassword = async function() {
             resolve(result.password);
         });
     });
-}
+};
 
 /** Run splitticketbuyer one time. */
 const runOnce = async function(password) {
@@ -112,25 +112,25 @@ const runOnce = async function(password) {
             "--wallet.host=127.0.0.1:0",
             "--sessionname=" + sessionName,
             "--maxamount=" + amount
-        ]
+        ];
 
         spawnSync(splitBuyerProgram, args, {stdio: 'inherit'});
         resolve(true);
     });
-}
+};
 
 /** Check if the the user has funds left to keep buying. */
 const hasFunds = async function() {
     // TODO: Implement this function to make an RPC to the wallet.
     return true;
-}
+};
 
 /** Sleep for the provided number of seconds. */
 const sleep = async function(seconds){
     return new Promise(resolve=>{
         setTimeout(resolve, seconds * 1000)
     });
-}
+};
 
 /** Main runloop. */
 const main = async function() {
@@ -150,6 +150,6 @@ const main = async function() {
             console.log("Exiting " + projectName);
         }
     }
-}
+};
 
 main();
